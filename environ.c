b782d178a6c018c071b05de8b847fbc047d05db1
Wanemodinaka <142806359+Wanemodinaka@users.noreply.github.com>
2023-11-15 15:27:04 +0100

    Update environ.c

--- environ.c
+++ environ.c
@@ -4,6 +4,7 @@
  * _myenv - prints the current environment
  * @info: Structure containing potential arguments. Used to maintain
  *          constant function prototype.
+ * By Givens and Gabriel
  * Return: Always 0
  */
 int _myenv(info_t *info)
