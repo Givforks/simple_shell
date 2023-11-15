0b1e2af9e1dcb76003350a9cffd94fe1822b61ee
Wanemodinaka <142806359+Wanemodinaka@users.noreply.github.com>
2023-11-15 15:23:36 +0100

    Update builtin.c

--- builtin.c
+++ builtin.c
@@ -3,6 +3,7 @@
  * _myexit - exits the shell
  * @info: Structure containing potential arguments. Used to maintain
  *          constant function prototype.
+ *  By Givens and Gabrriel
  *  Return: exits with a given exit status
  *         (0) if info.argv[0] != "exit"
  */
